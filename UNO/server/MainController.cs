ad9a9f89f1eebe3fd23181eb9ac670b4c30729bf
Krisz-Lenovo <bankairyuu@gmail.com>
2015-02-24 16:53:00 +0100

    pár osztály

---
+++ UNO/server/MainController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server
+{
+    class MainController
+    {
+        /// <summary>
+        /// <c>MSGBOX</c> A megjelenített szövegdoboz, melyen a kiíratás megtörténik, mintha csak egy sima console app lenne
+        /// </summary>
+        private System.Windows.Controls.TextBox MSGBOX;
+
+
+        public MainController(System.Windows.Controls.TextBox MSGBOX)
+        {
+            // TODO: Complete member initialization
+            this.MSGBOX = MSGBOX;
+        }
+    }
+}
