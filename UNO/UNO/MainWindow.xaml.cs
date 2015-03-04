d8bd4d8825258f27a030130825f5e5c8ca8be882
Sipos István Krisztián <bankairyuu@gmail.com>
2015-03-04 17:21:48 +0100

    Alakulóban van

--- UNO/UNO/MainWindow.xaml.cs
+++ UNO/UNO/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -22,11 +23,51 @@ namespace UNO
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Thread BroadcastSzal;
+        private Thread JatekSzal;
+        private TcpClient client = new TcpClient();
+        private IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 3000);
+
 
         public MainWindow()
         {
             InitializeComponent();
-            new MainController(MSGBOX);
+            MSGBOX.Text += System.Environment.NewLine + "### Connecting to the server ###" + System.Environment.NewLine;
+            client.Connect(serverEndPoint);
+            MSGBOX.Text += "### Connected to the server ###" + System.Environment.NewLine;
         }
+
+
+        /// <summary>
+        /// Itt kellene majd megoldani, hogy ne kiírja az MSGBOX-ba, hanem egy szálon felírja a szervernek. Ez a szál lesz majd a Játék szál!
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e">Milyen billentyűt ütött le a kliens a Commandhoz</param>
+        private void Input_field_KeyDown(object sender, KeyEventArgs e)
+        {
+            // e.KeyData != Keys.Enter || e.KeyData != Keys.Return
+            if (e.Key == Key.Enter)
+            {
+                MSGBOX.Text += Input_field.Text;
+                MSGBOX.Text += System.Environment.NewLine;
+                Input_field.Text = "";
+            }
+            else
+            {
+
+            }
+        }
+
+        /// <summary>
+        /// Ez lesz az a függvény, ami majd a szerverre hallgatózik, hogy az valamikor broadcastol-e
+        /// </summary>
+        private void ServerReading()
+        {
+
+        }
+
+        
+
+
     }
 }
---
+++ UNO/server/DebugMessageClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace server
+{
+    class DebugMessageClass
+    {
+
+        internal void ClientConnected(System.Windows.Controls.TextBox MSGBOX, int c)
+        {
+            string s = System.Environment.NewLine + ">> Client connected!" + System.Environment.NewLine
+                + ">> Amount of connected clients: " + c + System.Environment.NewLine;
+            MSGBOX.Text += s;
+            MessageBox.Show(s);
+        }
+
+        
+
+        internal void ServerStart(System.Windows.Controls.TextBox MSGBOX)
+        {
+            MSGBOX.Text = "### SERVER ###";
+            MSGBOX.Text += System.Environment.NewLine + "### Server started...###" + System.Environment.NewLine
+                + "### Waiting for clients ###" + System.Environment.NewLine;
+        }
+    }
+}
