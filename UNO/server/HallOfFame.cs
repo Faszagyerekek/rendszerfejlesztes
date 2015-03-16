965e4bc395a2d3ff958a3d50f9655b7537c5d76f
DasherLac <dasherlac@gmail.com>
2015-03-16 18:55:27 +0100

    player+HOF+record

---
+++ UNO/server/HallOfFame.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server
+{
+    class HallOfFame
+    {
+        private List<Record> records;
+
+        HallOfFame() { 
+            records =new List<Record>();
+        }
+        ///<summary></summary>
+        /// <param name="records">Az eltárolt bejegyzések</param>
+
+        public void addRecord(Record record)
+        {
+            records.Add(record);
+        }
+        //most nem írok remove-ot, mert eléletileg nem kell, talán majd extraként
+        // egyébként is: THE LEGEND NEVER DIES!
+    }
+}
---
+++ UNO/server/Record.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using game;
+namespace server
+{
+    class Record
+    {
+        private string _username;
+        private int _points;
+
+        public Record() { }
+
+        /// <summary>
+        /// Felüldefiniált konstruktor (felhasználónév, pontok)
+        /// </summary>
+        /// <param name="username">A játékos felhasználó neve</param>
+        /// <param name="points">A játékos pontjai</param>
+
+        public string username
+        {
+            set { this._username = value; }
+            get { return this._username; }
+        }
+        
+        public int points
+        {
+            set { this._points = value; }
+            get { return this._points; }
+        }
+
+        public Record(string username, int points)
+        {
+            this.username = username;
+            this.points = points;
+        }
+
+
+    }
+}
