1d8e01be8bc1640562d9565a9d5a8bede0157b49
Krisz-Lenovo <bankairyuu@gmail.com>
2015-03-28 17:57:32 +0100

    asdf

--- UNO/UNO/MainWindow.xaml.cs
+++ UNO/UNO/MainWindow.xaml.cs
@@ -112,8 +112,6 @@ namespace UNO
             
             string json = JsonConvert.SerializeObject(message);
 
-            _Log(json);
-
             UTF8Encoding encoder = new UTF8Encoding();
             byte[] buffer = encoder.GetBytes(json);
 
