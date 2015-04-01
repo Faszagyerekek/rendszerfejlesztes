9fa6e68644a51005319e7a09b40133ac5dca66b5
DasherLac <dasherlac@gmail.com>
2015-04-01 17:59:03 +0200

    Szim/szin check

--- UNO/server/RuleChecker.cs
+++ UNO/server/RuleChecker.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 using game;
 namespace server
 {
-    class RuleChecker
+    class RuleChecker//nincs is adattagja
     {
+        RuleChecker()//kell ide egyeltalán valami?
+        {
+
+        }
+
+        /// <summary>
+        /// két lapot kap: onTop, toPlace; lehet-e a toPlace-t a onTop-ra rakni
+        /// </summary>
+        /// <param name="onTop">dobópakli felső lapja</param>
+        /// <param name="toPlace">eldobni kívánt lap</param>
+
+        bool symColCheck(Card onTop, Card toPlace)
+        {
+            if (onTop.color.Equals("BLACK") ||
+                onTop.color.Equals(toPlace.color)||
+                onTop.symbol.Equals(toPlace.symbol)
+                )
+            {
+                return true;
+            }
+            return false;
+        }
+
+
     }
 }
