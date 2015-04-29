71a6e1a3ccec45213228a951a2f1d97cc3122128
Krisz-Lenovo <bankairyuu@gmail.com>
2015-04-29 18:21:07 +0200

    új GUI

---
+++ UNO/UNO_GUI/Card.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game
+{
+    public class Card
+    {
+        private string _type, _color, _symbol;
+        private int _penaltyPoint;
+
+        public Card() { }
+
+        /// <summary>
+        /// Felüldefiniált konstruktor (típus, szín, szimbólum, hibapont)
+        /// </summary>
+        /// <param name="type">A kártya típusa (általános, vagy speciális)</param>
+        /// <param name="color">A kártya színe</param>
+        /// <param name="symbol">A kártya száma, vagy szimbóluma</param>
+        /// <param name="penaltyPont">A kártyához tartozó hibapont</param>
+        public Card(string type, string color, string symbol, int penaltyPont = 0)
+        {
+            this.type = type;
+            this.color = color;
+            this.symbol = symbol;
+            this.penaltyPoint = penaltyPoint;
+        }
+
+        public string type
+        {
+            set { this._type = value; }
+            get { return this._type; }
+        }
+
+        public string color
+        {
+            set { this._color = value; }
+            get { return this._color; }
+        }
+
+        public string symbol
+        {
+            set { this._symbol = value; }
+            get { return this._symbol; }
+        }
+
+        public int penaltyPoint
+        {
+            set { this._penaltyPoint = value; }
+            get { return this._penaltyPoint; }
+        }
+
+        public bool Equals(Card card)
+        {
+            if (this.color == card.color 
+                && this.type == card.type 
+                && this.symbol == card.symbol
+                )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        /// <summary>
+        /// Szimbólum eqvivalencia vizsgálat
+        /// </summary>
+        /// <param name="card"> paraméter kártya</param>
+        /// <returns>ha a szimbólum egyezik, akkor true, amúgy false</returns>
+        public bool symEquals(Card card)
+        {
+            if (this.symbol == card.symbol){
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ UNO/UNO_GUI/Message.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using game;
+
+namespace server
+{
+    public class Head
+    {
+        public string STATUS { get; set; }
+        public string STATUSCODE { get; set; }
+        public string FROM { get; set; }
+        public string TO { get; set; }
+    }
+
+    public class Body
+    {
+        public game.Card CARD { get; set; }
+        public string MESSAGE { get; set; }
+    }
+
+    public class Message
+    {
+
+        public Message(string p, string username, string toWho, string msg)
+        {
+            this.head = new Head();
+            this.body = new Body();
+            // TODO: Complete member initialization
+            this.head.STATUS = p;
+            this.head.FROM = username;
+            this.head.TO = toWho;
+            this.body.MESSAGE = msg;
+        }
+
+        public Message(string p, string username, string toWho, Card card)
+        {
+            this.head = new Head();
+            this.body = new Body();
+            // TODO: Complete member initialization
+            this.head.STATUS = p;
+            this.head.FROM = username;
+            this.head.TO = toWho;
+            this.body.CARD = card;
+        }
+
+        public Message(string p, string statuscode, string username, string toWho, Card card)
+        {
+            this.head = new Head();
+            this.body = new Body();
+            // TODO: Complete member initialization
+            this.head.STATUS = p;
+            this.head.STATUSCODE = statuscode;
+            this.head.FROM = username;
+            this.head.TO = toWho;
+            this.body.CARD = card;
+        }
+
+        public Message(string p, string statuscode, string username, string toWho, string msg)
+        {
+            this.head = new Head();
+            this.body = new Body();
+            // TODO: Complete member initialization
+            this.head.STATUS = p;
+            this.head.STATUSCODE = statuscode;
+            this.head.FROM = username;
+            this.head.TO = toWho;
+            this.body.MESSAGE = msg;
+        }
+
+
+        public Message()
+        {
+            this.head = null;
+            this.body = null;
+        }
+        public Head head { get; set; }
+        public Body body { get; set; }
+    }
+}
